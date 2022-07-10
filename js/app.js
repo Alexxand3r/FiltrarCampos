5078e30a211614919681209d62ec15f3a9a42085
Alexander De Simone <cristianoali08i@gmail.com>
2022-07-09 23:06:21 -0300

    primer push

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 'use strict';
 
 //Variables
+const resultado = document.querySelector('#resultado');
+
+// eventos
+document.addEventListener('DOMContentLoaded', () => {
+  mostrarAutos();
+});
+
+//Funciones
+function mostrarAutos() {
+  autos.forEach(auto => {
+    const autoHTML = document.createElement('p');
+
+    autoHTML.textContent = `
+    ${auto.marca}
+    `;
+    //Insertar en el HTML
+    resultado.appendChild(autoHTML);
+  });
+}
